f2087730c58da773c626a68d3f6093103e3ade79
Kazagha <chrisg.local@gmail.com>
2015-09-07 14:41:32 +1000

    ItemMundane: Add basic craft check logic

--- src/ItemMundane.java
+++ src/ItemMundane.java
@@ -3,7 +3,7 @@ public class ItemMundane extends Item {
 	private int DC;
 	
 	public ItemMundane(String name, int baseCost, int DC) {		
-		super(name, baseCost, baseCost / 3);
+		super(name, baseCost * 10, baseCost / 3 );
 		this.DC = DC;
 	}
 	
@@ -18,7 +18,22 @@ public class ItemMundane extends Item {
 	}
 	
 	@Override
-	void update() {
-				
+	void update(Controller controller) {
+		String text = String.format("Roll craft check (DC=%s)", getDC());
+		int check = controller.check(text);	
+			
+		// Was the check successful
+		if(check > getDC()) 
+		{
+			// Successful check
+			setProgress(getProgress() + (getDC() * check));
+		} else if (check < getDC() - 4)
+		{
+			// Check Failed by 5 or more: Half raw materials have been destroyed
+			int cost = getMatCost() / 2;
+		}	
+		
+		System.out.format("DC: %s Check: %s", getDC(), check);			
+		notifyObservers();
 	}
 }
