83177d6b59d24594e9822ac16667e36da697f07c
Kazagha <chrisg.local@gmail.com>
2015-09-19 19:58:37 +1000

    SpellEffect: Add the 'create' and 'edit' methods

--- src/SpellEffect.java
+++ src/SpellEffect.java
@@ -1,21 +1,98 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
 
 public class SpellEffect extends Effect {
 
 	private String name;
 	private int casterLevel;
 	private int spellLevel;
-	private int cost;
-	private int XP;
+	private int materialCost;
+	private int xpCost;
 	
 	public SpellEffect() {}
 	
-	public SpellEffect(String name, int casterLevel, int spellLevel, int cost, int XP)
+	public SpellEffect(String name, int casterLevel, int spellLevel, int materialCost, int xpCost)
 	{
 		this.name = name;
 		this.casterLevel = casterLevel;
 		this.spellLevel = spellLevel;
+		this.materialCost = materialCost;
+		this.xpCost = xpCost;
 	}
 	
+	public static SpellEffect create()
+	{
+		SpellEffect newEffect = new SpellEffect("", 0, 0, 0, 0);
+		
+		if(newEffect.edit() == JOptionPane.OK_OPTION)
+			return newEffect;
+		
+		return null;
+	}
+	
+	public int edit()
+	{
+		ArrayList<Object> array = new ArrayList<Object>();
+		
+		JTextField name = new JTextField(getName());
+		JTextField casterLevel = new JTextField(getCasterLevel());
+		JTextField spellLevel = new JTextField(getSpellLevel());
+		JTextField materialCost = new JTextField(getMaterialCost());
+		JTextField xpCost = new JTextField(getXPCost());
+		
+		array.addAll(Arrays.asList(new Object[] { 
+				"Name", name, 
+				"Caster Level", casterLevel, 
+				"Spell Level", spellLevel, 
+				"Material Cost", materialCost, 
+				"XP Cost", xpCost 
+				}));
+		
+		int result = JOptionPane.OK_OPTION;
+		while(result == JOptionPane.OK_OPTION)
+		{
+			try
+			{
+				result = Controller.editArray(array);
+				
+				this.setName(name.getText());
+				this.setCasterLevel(Integer.valueOf(casterLevel.getText()));
+				this.setSpellLevel(Integer.valueOf(spellLevel.getText()));
+				this.setMaterialCost(Integer.valueOf(materialCost.getText()));
+				this.setXPCost(Integer.valueOf(xpCost.getText()));
+				return result;
+			} catch (Exception e) {
+				
+			}
+		}
+		
+		return result;
+	}
+	
+	public int getMaterialCost() 
+	{
+		return materialCost;
+	}
+
+	public void setMaterialCost(int materialCost) 
+	{
+		this.materialCost = materialCost;
+	}
+
+	public int getXPCost() 
+	{
+		return xpCost;
+	}
+
+	public void setXPCost(int xpCost) 
+	{
+		this.xpCost = xpCost;
+	}
+
 	public int getCasterLevel() 
 	{
 		return casterLevel;
@@ -40,29 +117,9 @@ public class SpellEffect extends Effect {
 	{
 		this.name = name;
 	}
-	
-	public void setCost(int cost) 
-	{
-		this.cost = cost;
-	}
-
-	public void setXP(int XP)
-	{
-		this.XP = XP;
-	}
 
 	@Override
 	public String getName() {
 		return this.name;
-	}
-	
-	@Override
-	public int getCost() {
-		return this.cost;
-	}
-
-	@Override
-	public int getXP() {
-		return this.XP;
 	}
 }
