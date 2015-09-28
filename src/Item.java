48b819290e1f4c659cc26f473948dbefad99afe4
Kazagha <chrisg.local@gmail.com>
2015-09-28 12:57:18 +1000

    Item: Remove the 'basePrice' and 'matPrice', instead use the abstract getter methods 'getPrice()' and 'getCraftPrice()'

--- src/Item.java
+++ src/Item.java
@@ -5,8 +5,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public abstract class Item  extends Observable {
 	private String name;
-	private int baseCost;
-	private int matCost;
 	private int progress;
 	private TYPE itemType;
 	
@@ -14,48 +12,29 @@ public abstract class Item  extends Observable {
 	
 	public Item() {}
 	
-	public Item(String name, int baseCost, int matCost) 
+	public Item(String name) 
 	{
 		this.name = name;
-		this.baseCost = baseCost;
-		this.matCost = matCost;
 	}
 	
 	public String getName() 
 	{
 		return this.name;		
 	}
-	
-	public int getBaseCost() 
-	{
-		return this.baseCost;
-	}
-	
-	public int getMatCost() 
-	{
-		return this.matCost;
-	}
-	
+		
 	public int getProgress() 
 	{
 		return this.progress; 
 	}	
 	
+	public TYPE getItemType()
+	{
+		return itemType;
+	}
+	
 	public void setName(String name) 
 	{
 		this.name = name;
-		setChanged();
-	}
-	
-	public void setBaseCost(int i)
-	{
-		this.baseCost = i;
-		setChanged();
-	}
-	
-	public void setMatCost(int i) 
-	{
-		this.matCost = i;
 		setChanged();
 	}
 	
@@ -64,26 +43,22 @@ public abstract class Item  extends Observable {
 		this.progress = i;
 		setChanged();
 	}
-	
-	public boolean isComplete() 
-	{
-		return progress >= baseCost;
-	}
-	
-	
-	public TYPE getItemType()
-	{
-		return itemType;
-	}
-	
-	
+			
 	public void setItemType(TYPE type) 
 	{
 		this.itemType = type;
 	}
-	
+
+	public boolean isComplete() 
+	{
+		return progress >= getCraftPrice();
+	}
 	
 	abstract void update();
 	
 	public abstract int edit();
+	
+	public abstract int getPrice();
+	
+	public abstract int getCraftPrice();
 }
