b4bf1da9a25fa5107d2c42cb0ad83c33f493199e
Kazagha <chrisg.local@gmail.com>
2015-09-30 19:25:12 +1000

    SelectFocusListener: Initial commit.

---
+++ src/SelectAllFocus.java
@@ -0,0 +1,34 @@
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class SelectAllFocus implements FocusListener
+{
+	@Override
+	public void focusGained(FocusEvent event) 
+	{		
+		SwingUtilities.invokeLater(new selectAll(event.getSource()));		
+	}
+
+	@Override
+	public void focusLost(FocusEvent event) {}
+	
+	private class selectAll implements Runnable
+	{
+		Object source;
+		
+		public selectAll(Object source)
+		{
+			this.source = source;
+		}
+
+		@Override
+		public void run() {
+			
+			if(this.source instanceof JTextField)
+				((JTextField) this.source).selectAll();
+		}		
+	}
+}
