6717ffd4b5a3b76bfbe90715d7ce0cbf5cbc92cc
Kazagha <chrisg.local@gmail.com>
2015-10-02 13:18:53 +1000

    EffectBonus: Expand the Type enum

--- src/EffectBonus.java
+++ src/EffectBonus.java
@@ -1,22 +1,40 @@
+import javax.swing.JOptionPane;
+
 
 public class EffectBonus extends Effect {
 
-	
-	
-	
-	enum type
+	enum Type
 	{
-		ABILITY_BONUS		("Ability Bonus (enhancement)", 1000);
+		ABILITY_BONUS		("Ability Bonus (enhancement)", 1000),
+		ARMOR_BONUS			("Armor bonus (enhancement)", 1000),
+		AC_BONUS_DEFLECTION ("AC bonus (deflection)", 2000),
+		AC_BONUS_OTHER		("AC bonus (other)", 2500),
+		AC_NATURAL			("Natural armor bonus (enhancement)", 2000),
+		SAVE_BONUS_RESIST	("Save bonus (resistance)", 1000),
+		SAVE_BONUS_OTHER	("Save bonus (other)", 2000),
+		SKILL_BONUS			("Skill bonus (competence)", 100),
+		WEAPON_BONUS		("Weapon bonus (enhancement)", 2000);		
 		
 		String desc;
-		int price;
-		type(String desc, int price)
+		int cost;
+		Type(String desc, int cost)
 		{
 			this.desc = desc;
-			this.price = price;
+			this.cost = cost;
 		}
 		
+		public String getDesc()
+		{
+			return desc;
+		}
+		
+		public int getCost()
+		{
+			return cost;
+		}		
 	}
+	
+	int bonus;
 	
 	public EffectBonus() {
 		// TODO Auto-generated constructor stub
