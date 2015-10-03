64acd08f4f36ef7e31f725c0ade277d1641ecf46
Kazagha <chrisg.local@gmail.com>
2015-10-03 15:01:05 +1000

    ItemMagicGeneric: Initial commit. Basic 'magic' item.

---
+++ src/ItemMagicGeneric.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+
+public class ItemMagicGeneric extends ItemMagic {
+	
+	private ArrayList<Effect> effect;
+
+	public ItemMagicGeneric() {}
+	
+	public ItemMagicGeneric(String name)
+	{
+		this.setName(name);
+	}
+	
+	@Override
+	ArrayList<Effect> getEffect() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void setEffect(ArrayList<Effect> effect) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void addEffect(Effect effect) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	int getXP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int edit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCraftPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
