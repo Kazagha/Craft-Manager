8c69793840b8f65c406a7d5d480d5400ff2ab7db
Kazagha <chrisg.local@gmail.com>
2015-10-03 18:48:41 +1000

    ItemMagicBasic: Implement the 'edit' method

--- src/ItemMagicBasic.java
+++ src/ItemMagicBasic.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
 
 public class ItemMagicBasic extends ItemMagic {
 	
@@ -12,12 +15,30 @@ public class ItemMagicBasic extends ItemMagic {
 		this.setName(name);
 	}
 	
-	public static ItemMagicBasic
-
 	@Override
 	public int edit() 
 	{
-		return 1;
+		Object[] array;
+		
+		JTextField name = new JTextField(this.getName());
+		
+		array = new Object[] { "Name", name };
+		
+		int result = JOptionPane.OK_OPTION;
+		while(result == JOptionPane.OK_OPTION)
+		{
+			try {
+				result = Controller.getInstance().editArray(array);
+				
+				// Set changes
+				this.setName(name.getText());
+				return result;
+			} catch (Exception e) {
+				Controller.getInstance().showMessage("Input Error: " + e.getMessage());
+			}
+		}
+		
+		return result;
 	}
 
 	@Override
