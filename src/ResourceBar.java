3be8ef130f170be18dc933957db2d4f1619b3cce
Kazagha <chrisg.local@gmail.com>
2015-10-20 15:12:08 +1000

    ResourceBar: Add line above and below the resource bar

--- src/ResourceBar.java
+++ src/ResourceBar.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -12,18 +14,19 @@ public class ResourceBar extends JPanel {
 	
 	public ResourceBar()
 	{
-		this.setLayout(new FlowLayout(FlowLayout.CENTER));
-		this.add(gpLabel);
-		this.add(new JLabel(gpImage));
-		this.add(xpLabel);		
+		initComponents();		
 	}
 	
 	private void initComponents()
 	{
 		gpLabel = new JLabel();
-		xpLabel = new JLabel();
+		xpLabel = new JLabel();		
+		gpImage = new ImageIcon("images/gold.png");	
 		
-		gpImage = new ImageIcon("images/gold.png");		
+		this.setLayout(new FlowLayout(FlowLayout.CENTER));
+		this.add(gpLabel);
+		this.add(new JLabel(gpImage));
+		this.add(xpLabel);
 	}
 	
 	public void setGP(int num)
@@ -34,5 +37,16 @@ public class ResourceBar extends JPanel {
 	public void setXP(int num)
 	{
 		xpLabel.setText(num + " XP");
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		
+		int y = 0;
+		g.setColor(new Color(156, 154, 140));
+		g.drawLine(0, y, getWidth(), y);
+		g.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
 	}
 }
