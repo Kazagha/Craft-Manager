ecb1888836638b3f0f394fc0dfac61af47b4e769
Kazagha <chrisg.local@gmail.com>
2015-10-24 16:36:20 +1000

    DragMouseAdapter: Implement the 'mousePressed' method. This will set startPt and check if there are at least two components to swap places

--- src/DragMouseAdapter.java
+++ src/DragMouseAdapter.java
@@ -1,5 +1,35 @@
+import java.awt.Color;
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JComponent;
+import javax.swing.JWindow;
 
 public class DragMouseAdapter extends MouseAdapter {
+	
+	private final JWindow window = new JWindow();
+	private Point startPt;
 
+	public DragMouseAdapter()
+	{
+		super();
+		// Set the background color with transparency
+		window.setBackground(new Color(0, true));
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		JComponent parent = (JComponent) e.getComponent();
+		
+		// Check if there are at least two components to select 
+		if (parent.getComponentCount() <= 1)
+		{
+			startPt = null;
+			return;
+		}
+		
+		startPt = e.getPoint();			
+	}
 }
