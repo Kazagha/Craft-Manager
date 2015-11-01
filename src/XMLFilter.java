e7a2ed68a3e6d0554fe8c8ddcfbe814e141cc04d
Kazagha <chrisg.local@gmail.com>
2015-11-02 09:12:40 +1000

    XMLFilter: Initial commit. Used with JFileChooser to select only XML files

---
+++ src/XMLFilter.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
+
+public class XMLFilter extends FileFilter {
+	
+	public XMLFilter()
+	{
+		super();
+	}
+
+	@Override
+	public boolean accept(File arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
