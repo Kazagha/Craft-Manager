dbfb9de425203f7b1528cdd270c684af9805ae5f
Kazagha <chrisg.local@gmail.com>
2015-11-02 09:29:57 +1000

    XMLFilter: Implement the 'accept' and 'getDescription' methods

--- src/XMLFilter.java
+++ src/XMLFilter.java
@@ -12,15 +12,36 @@ public class XMLFilter extends FileFilter {
 	}
 
 	@Override
-	public boolean accept(File arg0) {
-		// TODO Auto-generated method stub
+	public boolean accept(File f) 
+	{
+		// Show directories
+		if (f.isDirectory())
+			return true;
+		
+		// Fetch the file name
+		String name = f.getName();
+		String extension = null;
+		
+		// Find the '. file extension' 
+		int index = name.lastIndexOf(".");		
+		if (index > 0)
+			extension = name.substring(index);
+		
+		// Check if the extension exists
+		if (extension != null)
+		{
+			// Return true if the file extension is 'CSV'
+			if (extension.equalsIgnoreCase(".xml"))
+				return true;
+		}
+	
 		return false;
 	}
 
 	@Override
-	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getDescription() 
+	{
+		return "XML (EXtensible Markup Language)";
 	}
 
 }
