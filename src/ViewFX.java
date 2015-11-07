ea453bb28be4fb71c3dc371fead02fbc5c15100f
Kazagha <chrisg.local@gmail.com>
2015-11-07 16:42:28 +1000

    ViewFX: Create 'init' method
    This will bundle the setup of Scene, instantiate components and set
    setOnAction events

--- src/ViewFX.java
+++ src/ViewFX.java
@@ -4,21 +4,32 @@ import java.util.Observer;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-
 
 public class ViewFX implements Observer {
 
-	Scene scene;
+	private Scene scene;
+	private GridPane grid;
+	private Button test;
 	
 	final int SCENE_WIDTH = 450;
 	final int SCENE_HEIGHT = 650;
 	
 	public ViewFX()
 	{
-		GridPane grid = new GridPane();		
+		init();	
 		
+		grid.add(test, 0, 0);
+	}
+	
+	public void init()
+	{	
+		grid = new GridPane();
 		scene = new Scene(grid, SCENE_WIDTH, SCENE_HEIGHT);
+		
+		test = new Button(ControllerFX.Action.ADDGOLD.toString());
+		test.setOnAction(actionEvent -> ControllerFX.getInstance().addGold());
 	}
 	
 	private void removeAllItems() {}
