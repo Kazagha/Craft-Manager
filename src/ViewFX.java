2aac50cb413f74687da623ca0e4c64339d5d0b22
Kazagha <chrisg.local@gmail.com>
2015-11-28 12:50:15 +1000

    ViewFX: Outline new design
    Begin general outline of the new design

--- src/ViewFX.java
+++ src/ViewFX.java
@@ -5,36 +5,41 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 public class ViewFX implements Observer {
 
 	private Scene scene;
-	private GridPane grid;
 	private Button test;
 	
 	final int SCENE_WIDTH = 450;
-	final int SCENE_HEIGHT = 650;
+	final int SCENE_HEIGHT = 700;
 	
 	public ViewFX()
 	{
-		init();	
-		
-		
+		init();			
 	}
 	
 	public void init()
-	{	
-		grid = new GridPane();
-		scene = new Scene(grid, SCENE_WIDTH, SCENE_HEIGHT);
+	{		
+		// Set the Scene and style sheet
+		BorderPane root = new BorderPane();
+		scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+		scene.getStylesheets().add(
+				ViewFX.class.getResource("ViewFX.css").toExternalForm());
 		
-		test = new Button(ControllerFX.Action.ADDGOLD.toString());
-		test.setOnAction(actionEvent -> ControllerFX.getInstance().addGold());		
-		grid.add(test, 0, 0);
+		// Set the Top/Center/Bottom panes
+		HBox top = new HBox();
+		root.setTop(top);
 		
-		Button button = new Button(ControllerFX.Action.NEWMAGICITEM.toString());
-		button.setOnAction(actionEvent -> ControllerFX.getInstance().newItemMagic());
-		grid.add(button, 0, 1);
+		StackPane center = new StackPane();
+		root.setCenter(center);
+		
+		HBox bottom = new HBox();
+		root.setBottom(bottom);
 	}
 	
 	private void removeAllItems() {}
