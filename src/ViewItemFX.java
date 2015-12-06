da2b2abbbd7397a07bbacb4c40cbfd98f0366395
Kazagha <chrisg.local@gmail.com>
2015-12-06 14:08:53 +1000

    ViewItemFX: Add Setter methods
    Set progress and item name

--- src/ViewItemFX.java
+++ src/ViewItemFX.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
@@ -25,7 +26,23 @@ public class ViewItemFX extends Pane
 		HBox h = new HBox();
 		VBox v = new VBox();
 		
+		this.getChildren().add(h);
 		h.getChildren().addAll(icon, v);
 		v.getChildren().addAll(name, progress);
+	}
+	
+	public void setName(String str)
+	{
+		this.name.setText(str);
+	}
+	
+	public void setProgressBar(int value, int total)
+	{
+		if (total == 0) {
+			progress.setProgress(0);
+			return;
+		}
+		
+		progress.setProgress(value/total);
 	}
 }
