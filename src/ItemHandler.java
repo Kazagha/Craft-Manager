45ed2524290c01628017ec2e9d8b942128269820
Kazagha <chrisg.local@gmail.com>
2015-12-09 17:39:54 +1000

    ItemHandler: Initial commit
    Event handler for the Items in the view

---
+++ src/ItemHandler.java
@@ -0,0 +1,13 @@
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class ItemHandler implements EventHandler {
+
+	public ItemHandler() {}
+	
+	@Override
+	public void handle(Event event)
+	{
+		
+	}
+}
