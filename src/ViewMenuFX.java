f0453462d1b76fc7f8002f3043bfbff6b032fd38
Kazagha <chrisg.local@gmail.com>
2015-12-14 11:07:42 +1000

    ViewMenuFX: Initial commit
    Move the menu in the center of the view into a separate class

---
+++ src/ViewMenuFX.java
@@ -0,0 +1,49 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class ViewMenuFX extends VBox {
+
+	private Text nameText;
+	private Text gpText;
+	private Text xpText;
+	private Button craftButton;
+	
+	public ViewMenuFX()
+	{
+		init();
+	}
+	
+	public void init() 
+	{
+		nameText = new Text();
+		gpText = new Text();
+		gpText.prefWidth(200);
+		xpText = new Text();
+		xpText.prefWidth(200);
+		craftButton = new Button("Craft");
+		
+		this.setStyle("-fx-background-color: LIGHTBLUE");
+		this.setAlignment(Pos.TOP_CENTER);		
+		this.getChildren().addAll(
+				nameText,
+				new HBox(gpText, xpText),
+				craftButton
+				);
+	}
+	
+	public void setItem(Item item)
+	{
+		nameText.setText(item.getName());
+		gpText.setText(String.format("%d gp",item.getCraftPrice()));
+		
+		if (item instanceof ItemMagic) {
+			ItemMagic itemM = (ItemMagic) item;
+			xpText.setText(String.format("%d XP", itemM.getXP()));
+		} else {
+			xpText.setText(String.format("0 XP"));
+		}			
+	}
+}
